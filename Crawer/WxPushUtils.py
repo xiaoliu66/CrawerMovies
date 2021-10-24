d4f894faca8285b88838a40646174af87adc331e
xiaoliu66 <lunhuilk@163.com>
2021-10-24 21:40:22 +0800

    添加图文消息

--- Crawer/WxPushUtils.py
+++ Crawer/WxPushUtils.py
@@ -38,7 +38,7 @@ def send_text_message(access_token, agentid, content):
             "content": content
         }
     }
-    print(send_message_url + "access_token=" + access_token)
+    # print(send_message_url + "access_token=" + access_token)
     post = requests.post(send_message_url + access_token, json.dumps(data))
     print(post.json())
 
@@ -88,3 +88,32 @@ def send_markdown_message(access_token, agentid, content):
     response = requests.post(send_message_url + access_token, json.dumps(data))
     return response.json()
 
+def send_news_messsage(access_token, agentid, title, description, url, picurl):
+    """
+    发送图文消息
+    :param access_token: 进入系统凭证
+    :param agentid: 应用id
+    :param title: 标题，不超过128个字节，超过会自动截断（支持id转译）
+    :param description: 描述，不超过512个字节，超过会自动截断（支持id转译）
+    :param url: 点击后跳转的链接。 最长2048字节，请确保包含了协议头(http/https)，小程序或者url必须填写一个
+    :param picurl: 图文消息的图片链接，支持JPG、PNG格式，较好的效果为大图 1068*455，小图150*150。
+    :return:
+    """
+    data = {
+        "touser": "@all",
+        "msgtype": "news",
+        "agentid": agentid,
+        "news": {
+            "articles": [
+                {
+                    "title": title,
+                    "description": description,
+                    "url": url,
+                    "picurl": picurl
+                }
+            ]
+        }
+    }
+
+    response = requests.post(send_message_url + access_token, json.dumps(data))
+    return response.json()
